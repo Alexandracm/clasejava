1e48261e510fdb6b47d019d35046de66a455138a
Pedro <phmb1234docs@gmail.com>
2018-09-24 16:56:31 -0500

    version inicial

---
+++ script/app.js
@@ -0,0 +1,68 @@
+/*
+var numero1=10.5;
+var numero2=5;
+var resultado= numero1*numero2;
+console.log (resultado);
+
+var nombre ='Alexandra ';
+var profesion ="Desarrollador Full Stack";
+console.log (nombre+profesion);
+
+var tiene_mascota =true;
+console.log (tiene_mascota);
+var datos=["juan", 26,false,25,78,"Desarrollador"];
+console.log(datos[1]);
+
+var edad=20;
+if (edad>17){
+	console.log("Eres mayor de edad");
+}
+ 
+var edad=18;
+if (edad>=18){
+	console.log("Eres mayor de edad");
+}
+
+var edad=41;
+if (edad>=18&& edad<=40){
+	console.log ("Eres mayor de edad");
+}else{
+
+	console.log("No Eres un adulto mayor");
+}
+
+
+/* ciclos  Estructura FOR
+
+
+var dias=['lunes', 'Martes' ,'miercoles' ,'jueves' ,'viernes' ,'sabado', 'domingo'];
+
+for (var i= 0; i< 7;i++){
+	console.log(dias[i]);
+}
+
+
+function suma(numero1,numero2) {
+	var resultado= numero1+numero2;
+	return resultado;
+	
+}
+
+/*invocar
+
+var res=suma(88,55);
+console.log(res);
+
+*/
+
+
+
+function iva (precio producto) {
+	var iva_producto=precio_producto*0.19;
+	return iva_producto;
+
+
+	var res= iva(1000);
+	console.log (res);
+	
+
