9ea2e0a5bbf103ab118ac37befb577b550db90ab
Pedro <phmb1234docs@gmail.com>
2018-09-25 15:55:51 -0500

    eventosjs

--- script/app.js
+++ script/app.js
@@ -53,7 +53,7 @@ function suma(numero1,numero2) {
 var res=suma(88,55);
 console.log(res);
 
-*/
+
 
 
 
@@ -64,5 +64,53 @@ function iva (precio producto) {
 
 	var res= iva(1000);
 	console.log (res);
+
+
+
+	var titulos= document.getElementsByTagName('h1');
+	console.log (titulos);
+
+   var clasetexto= document.getElementsByClassName('texto');
+   console.log (clasetexto);
+
+
+   var sede= document.getElementsByid('sede');
+console.log(sede);
+
+
+*/
+
+
+
+function iva (precio_producto,porcentaje_iva) {
+	var iva_producto=precio_producto*(porcentaje_iva/100);
+	return iva_producto;
+}
+
+function calcular_iva(){
+var precio= parseInt(document.getElementById('precio').value);
+var porcentaje_iva= parseInt(document.getElementById('iva').value);
+var resultado=iva(precio,porcentaje_iva);
+var salida= document.getElementById('resultado');
+salida.value=resultado;
+console.log(resultado);
+
+
+
+/*
+console.log(precio);
+console.log (porcentaje_iva);
 	
+*/
+}
+
+
+
+
+
+
+
+
+
+
 
